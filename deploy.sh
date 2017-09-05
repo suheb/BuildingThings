4dba63d44d9e222270541db9da998abd936a3bf5
Suhaib Khan <suheb.work@gmail.com>
2017-09-05 20:42:27 +0530

    Adding deploy script
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [[ $1 == "-m" ]]; then
+	msg=$2
+else
+	msg="Updading site"
+fi
+
+echo "Genrating content..."
+pelican content -o output -s pelicanconf.py
+echo "Updading gh-pages branch.."
+ghp-import output -m "$msg"
+echo "Pushing to user page repo.."
+env -i git push https://github.com/suheb/suheb.github.io.git gh-pages:master
+echo "Success!"

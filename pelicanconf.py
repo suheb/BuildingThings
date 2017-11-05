642afcb6a65145bce813f1c98b46cefe0ea42d8c
Suhaib Khan <suheb.work@gmail.com>
2017-11-05 13:16:31 +0530

    Updating links
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Suhaib Khan'
 SITENAME = 'Building things'
-SITEURL = ''
+SITEURL = '/'
 
 PATH = 'content'
 
@@ -20,9 +20,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Home', 'http://suheb.in/'),
-         ('Blog', 'http://suheb.in/blog'),
-         ('About', 'http://suheb.in/about'),
+MENUITEMS = (('Home', '/'),
+         ('About', '/about'),
          ('Resume', 'http://suheb.in/resume'),)
 
 # Social widget
